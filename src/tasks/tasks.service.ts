66f522666a03116d75c8c2916cde58f79a40988e
Pablo Vasquez <jptabares31@gmail.com>
2021-07-03 13:00:57 -0500

    Type safety added

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
 
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: Task[] = [];
 
-  public getTasks() {
+  public getTasks(): Task[] {
     return this.tasks;
   }
 }
