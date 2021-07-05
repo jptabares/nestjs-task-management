89bc2324534b39814f22074b3e4bbb291a5974b7
Pablo Vásquez <jptabares@Pablos-MacBook-Air.local>
2021-07-04 23:53:23 -0500

    Added validation for GET Task Search filters

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,8 +1,12 @@
-import { IsEnum } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  @IsOptional()
   @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
   search?: string;
 }
