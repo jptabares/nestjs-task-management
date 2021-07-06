b47daf1abe4442be68cfe722ad99e2c4be84ce2a
Pablo Vásquez <jptabares31@gmail.com>
2021-07-05 21:11:14 -0500

    Added validation to signUp function

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
@@ -9,6 +13,13 @@ export class UsersRepository extends Repository<User> {
   ): Promise<void> {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code == 23505) {
+        throw new ConflictException('Username already exists');
+      }
+      throw new InternalServerErrorException();
+    }
   }
 }
