57dbe377a158578c7d35c1e5b53d90bd8cbf9deb
Pablo Vásquez <jptabares31@gmail.com>
2021-07-05 22:15:30 -0500

    Adding signIn

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UsersRepository) private usersRepository: UsersRepository,
   ) {}
 
-  public async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+  public signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentialsDto);
+  }
+
+  public async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.usersRepository.findOne({ username });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'success';
+    }
+    throw new UnauthorizedException('Please check your login credendials');
   }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -5,6 +5,7 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
@@ -12,10 +13,15 @@ export class UsersRepository extends Repository<User> {
     authCredentialsDto: AuthCredentialsDto,
   ): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
     try {
       await this.save(user);
     } catch (error) {
+      console.log(error);
       if (error.code == 23505) {
         throw new ConflictException('Username already exists');
       }
